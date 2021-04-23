b7ec55d6b477f2904891c11f4b9fea1b9c42a9b2
diayan <diayans@outlook.com>
2021-04-23 13:32:35 +0000

    design end fast ui[wip]

---
+++ Crunchhour/Model/EmojiFeeling.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiFeeling.swift
+//  Crunchhour
+//
+//  Created by diayan siat on 23/04/2021.
+//
+
+import Foundation
+
+struct EmojiFeeling: Identifiable{
+    let id = UUID()
+    let emoji: String
+    let description: String
+}
---
+++ Crunchhour/Views/Helpers/EmojiView.swift
@@ -0,0 +1,36 @@
+//
+//  EmojiView.swift
+//  Crunchhour
+//
+//  Created by diayan siat on 23/04/2021.
+//
+
+import SwiftUI
+
+struct EmojiView: View {
+    let emojiFeeling: EmojiFeeling
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Text(emojiFeeling.emoji)
+                    .shadow(radius: 3)
+                    .font(.largeTitle)
+                    .frame(width: 50, height: 50, alignment: .center)
+                    .overlay(
+                        Circle().stroke(Color.yellow)
+                    )
+            }
+            Text(emojiFeeling.description)
+                .bold()
+                .foregroundColor(.secondary)
+                .padding(4)
+        }
+    }
+}
+
+struct EmojiView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiView(emojiFeeling: EmojiFeeling(emoji: "🥺", description: "Ok"))
+    }
+}
